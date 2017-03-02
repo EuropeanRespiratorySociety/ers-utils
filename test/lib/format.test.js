287b214e74a4ae55c7211ee4403b0aa6268df8df
idealley <samuelpouyt@gmail.com>
2017-03-02 18:32:02 +0100

    Parser improvement and new tests

--- test/lib/format.test.js
+++ test/lib/format.test.js
@@ -16,28 +16,40 @@ describe('Format Util', function() {
   });
 
   it('converts markdown to html', () => {
-    const toParse = ['body' ];
-    const childrenToParse = ['object', 'arrayOfObjects']
+    const toParse = ['body', 'bodyFalse', 'notInModel' ];
+    const childrenToParse = ['object','objectWithNoValueOfInterest', 'arrayOfObjects', 'emptyObject', 'emptyArray', 'false']
     const item = {
       body: "### This is a title\n",
+      bodyFalse: false,
       object: { info: "test string",
                 text: "test string",
                 foreign: "should not be parsed" },
+      objectWithNoValueOfInterest: {
+        something: 'string for fun'
+      },          
+      emptyObject: {},          
+      emptyArray: [], 
+      false: false,         
       arrayOfObjects: [
           { info: "test string" },
           { text: "test string" },
           { info: "test string" },
           { foreign: "should not be parsed" }
         ],
-        should: "not be touched either"
+        should: "not be touched either",
+        not: () => undefined 
       };
 
     let parsed = format.markDownToHtml(item, toParse, childrenToParse)
-
+console.log(parsed);
     assert.equal(parsed.body, '<h3>This is a title</h3>\n');
     assert.equal(parsed.object.info, '<p>test string</p>\n');
     assert.equal(parsed.object.text, '<p>test string</p>\n');
     assert.equal(parsed.object.foreign, 'should not be parsed');
+    assert.equal(parsed.objectWithNoValueOfInterest.something, 'string for fun');
+    assert.deepEqual(parsed.emptyObject, {});
+    assert.deepEqual(parsed.emptyArray, []);
+    assert.equal(parsed.false, false);
     assert.equal(parsed.arrayOfObjects[0].info, '<p>test string</p>\n');
     assert.equal(parsed.arrayOfObjects[1].text, '<p>test string</p>\n');
     assert.equal(parsed.arrayOfObjects[2].info, '<p>test string</p>\n');
