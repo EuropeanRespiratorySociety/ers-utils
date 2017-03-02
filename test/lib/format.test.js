8a71b8d93b8b60273b0d593bdc7fe5ea71e5280f
idealley <samuelpouyt@gmail.com>
2017-03-02 14:50:28 +0100

    lodash introduction

--- test/lib/format.test.js
+++ test/lib/format.test.js
@@ -24,15 +24,18 @@ describe('Format Util', function() {
                 text: "test string" },
       arrayOfObjects: [
           { info: "test string" },
+          { text: "test string" },
           { info: "test string" }
         ]
       };
     let parsed = format.markDownToHtml(item, toParse, childrenToParse)
+    console.log(parsed);
     assert.equal(parsed.body, '<h3>This is a title</h3>\n');
     assert.equal(parsed.object.info, '<p>test string</p>\n');
     assert.equal(parsed.object.text, '<p>test string</p>\n');
     assert.equal(parsed.arrayOfObjects[0].info, '<p>test string</p>\n');
-    assert.equal(parsed.arrayOfObjects[1].info, '<p>test string</p>\n');
+    assert.equal(parsed.arrayOfObjects[1].text, '<p>test string</p>\n');
+    assert.equal(parsed.arrayOfObjects[2].info, '<p>test string</p>\n');
   });
 
 
@@ -42,4 +45,22 @@ describe('Format Util', function() {
     const result = {title: 'Title to test', body: false, any: false}
     assert.deepEqual(format.mapModel(model,data), result);
   });
+
+  it('truncates a string and appends ... after the default length (80)', () =>{
+    const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a mi ut nunc rhoncus pharetra at ut neque. Nulla facilisi.";//125 characters
+    const truncated = format.truncate(string);
+    assert.isAtMost(truncated.length, 80);
+    assert.isTrue(truncated.includes('...'));
+  });
+
+  it('truncates a string and appends ... after the desired length', () =>{
+    const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a mi ut nunc rhoncus pharetra at ut neque. Nulla facilisi.";//125 characters
+    assert.isAtMost(format.truncate(string, 50).length, 50);
+  });
+
+  it('truncates a string and appends a custom ommission', () => {
+    const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a mi ut nunc rhoncus pharetra at ut neque. Nulla facilisi.";//125 characters
+    assert.isTrue(format.truncate(string, 50, '///').includes('///'));
+  });
+
 });
