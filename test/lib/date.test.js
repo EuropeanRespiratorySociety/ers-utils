553bbd82dd6be62849936adb70b78a659564133b
idealley <samuelpouyt@gmail.com>
2017-03-10 08:45:31 +0100

    Added new methods, es6 refactoring

--- test/lib/date.test.js
+++ test/lib/date.test.js
@@ -7,13 +7,13 @@ const moment = require('moment');
 
 describe('Date Util', function() {
   it('returns an object with calendar properties ', () => {
-    const timestamp = moment('10/03/2017', 'DD/MM/YYYY').format('x')
+    const timestamp = moment('03/10/2017', 'MM/DD/YYYY').format('x')
     const object = { 
             year: "2017", 
             month: "March", 
             timestamp: timestamp
         };
-    assert.deepEqual(date.calendar('10/03/2017'), object);
+    assert.deepEqual(date.calendar('03/10/2017'), object);
   });
 
   it('belongs to the calendar', () => {
@@ -50,40 +50,52 @@ describe('Date Util', function() {
   });
 
   it('formats a single date', () => {  
-    assert.equal(date.ersDate('12/02/2017'), '12 February, 2017');
-    assert.equal(date.ersDate('08/02/2017'), '8 February, 2017', 'There is no 0 in from of single digit day');
+    assert.equal(date.ersDate('02/12/2017'), '12 February, 2017');
+    assert.equal(date.ersDate('02/08/2017'), '8 February, 2017', 'There is no 0 in from of single digit day');
   });
 
   it('formats two dates in the same month', () => {  
-    assert.equal(date.ersDate('12/02/2017', '14/02/2017'), '12-14 February, 2017');
+    assert.equal(date.ersDate('02/12/2017', '02/14/2017'), '12-14 February, 2017');
   });
 
   it('formats two dates over two months', () => {  
-    assert.equal(date.ersDate('31/03/2017', '03/04/2017'), '31 March - 3 April, 2017');
+    assert.equal(date.ersDate('03/31/2017', '04/03/2017'), '31 March - 3 April, 2017');
   });
 
   it('returns a timestamp', () => {
-    const timestamp = moment('10/03/2017', 'DD/MM/YYYY').format('x')
-    assert.equal(date.toTimeStamp('10/03/2017'), timestamp);
+    const timestamp = moment('03/10/2017', 'MM/DD/YYYY').format('x')
+    assert.equal(date.toTimeStamp('03/10/2017'), timestamp);
   });
 
    
   it('returns an object with date fields formated', () => { 
-    const eventDate = '10/03/2017';
-    const eventEndDate = '11/03/2017';
-    const timestamp = moment('10/03/2017', 'DD/MM/YYYY').format('x')
+    const eventDate = '03/10/2017';
+    const eventEndDate = '03/11/2017';
+    const timestamp = moment('03/10/2017', 'MM/DD/YYYY').format('x')
     assert.deepEqual(date.dates(eventDate, eventEndDate), {
                 eventDates: '10-11 March, 2017',
                 startDateTimestamp: timestamp,
                 startDate: '10 March, 2017',
                 endDate: '11 March, 2017'
             });
+  });
+
+  it('returns an object with date fields formated long range!', () => { 
+    const eventDate = '01/09/2016';
+    const eventEndDate = '05/31/2017';
+    const timestamp = moment('01/09/2016', 'MM/DD/YYYY').format('x')
+    assert.deepEqual(date.dates(eventDate, eventEndDate), {
+                eventDates: '9 January - 31 May, 2017',
+                startDateTimestamp: timestamp,
+                startDate: '9 January, 2016',
+                endDate: '31 May, 2017'
+            });
   });  
 
   it('returns an object with date fields formated when there is no end date', () => { 
-    const eventDate = '10/03/2017';
+    const eventDate = '03/10/2017';
     const eventEndDate = false;
-    const timestamp = moment('10/03/2017', 'DD/MM/YYYY').format('x')
+    const timestamp = moment('03/10/2017', 'MM/DD/YYYY').format('x')
     assert.deepEqual(date.dates(eventDate, eventEndDate), {
                 eventDates: '10 March, 2017',
                 startDateTimestamp: timestamp,
@@ -92,23 +104,23 @@ describe('Date Util', function() {
   });
 
   it('is already passed', () => {  
-    assert.isTrue(date.isAlreadyPassed('10/02/2017'));
+    assert.isTrue(date.isAlreadyPassed('02/10/2017'));
   });
   
   it('is not already passed', () => {  
-    assert.isFalse(date.isAlreadyPassed(moment().add(7, 'days').format('DD/MM/YYYY')));
+    assert.isFalse(date.isAlreadyPassed(moment().add(7, 'days').format('MM/DD/YYYY')));
   });
 
   it('is already passed (LEGACY)', () => {  
-    assert.isNull(date.isAlreadyPassedLegacy('10/02/2017'));
+    assert.isNull(date.isAlreadyPassedLegacy('02/10/2017'));
   });
   
   it('is not already passed (LEGACY)', () => { 
-    const nowInFuture = moment().add(7, 'days').format('D/MM/YYYY');
+    const nowInFuture = moment().add(7, 'days').format('MM/D/YYYY');
     assert.equal(date.isAlreadyPassedLegacy(
         moment()
         .add(7, 'days')
-        .format('DD/MM/YYYY')), 
+        .format('MM/DD/YYYY')), 
         date.ersDate(nowInFuture)
         );
   });
--- test/lib/format.test.js
+++ test/lib/format.test.js
@@ -82,4 +82,112 @@ describe('Format Util', function() {
     assert.isTrue(format.truncate(string, 50, '///').includes('///'));
   });
 
+  it('formats an url to document (static path)', () => {
+    const attachement = {
+      "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/989cff0ca845c2185427",
+      "id": "989cff0ca845c2185427",
+      "title": "Practical_Information_EBUS_Part_2.pdf",
+      "qname": "o:989cff0ca845c2185427",
+      "typeQName": "n:node"
+    };
+    const baseUrl = 'https://www.ersnet.org/assets';
+    assert.equal(format.attachementUrl(baseUrl, 'static', attachement ), 'https://www.ersnet.org/assets/static?node=989cff0ca845c2185427');
+
+  });
+
+  it('formats an url to an image (preview path)', () => {
+    const attachement = {
+      "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+      "id": "daa976116100734310f3",
+      "title": "EBUS training.jpg",
+      "qname": "o:daa976116100734310f3",
+      "typeQName": "n:node"
+    };
+    const baseUrl = 'https://www.ersnet.org/assets';
+    assert.equal(format.attachementUrl(baseUrl, 'preview', attachement, 256 ), 'https://www.ersnet.org/assets/preview?node=daa976116100734310f3&name=img256&size=256');
+  });
+
+  it('returns formated urls', () => {
+    const item = {
+      "image": {
+        "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+        "id": "daa976116100734310f3",
+        "title": "EBUS training.jpg",
+        "qname": "o:daa976116100734310f3",
+        "typeQName": "n:node"
+      },
+      "highResImage": {
+        "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+        "id": "daa976116100734310f3",
+        "title": "EBUS training.jpg",
+        "qname": "o:daa976116100734310f3",
+        "typeQName": "n:node"
+      },
+      "programme": {
+        "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+        "id": "daa976116100734310f3",
+        "title": "EBUS training.jpg",
+        "qname": "o:daa976116100734310f3",
+        "typeQName": "n:node"
+      },
+      "practicalInfo": {
+        "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+        "id": "daa976116100734310f3",
+        "title": "EBUS training.jpg",
+        "qname": "o:daa976116100734310f3",
+        "typeQName": "n:node"
+      },
+      "sponsor": [
+        {
+        "text": "this is the first string",
+        "image": {
+            "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+            "id": "daa976116100734310f3",
+            "title": "EBUS training.jpg",
+            "qname": "o:daa976116100734310f3",
+            "typeQName": "n:node"
+          },
+        },
+        {
+        "text": "this is the second string",
+        "image": {
+            "ref": "node://18dbd4f08d5f428ba9c2/607e97e4474d46e40345/b6b2871b2d9cf6b4996b/daa976116100734310f3",
+            "id": "daa976116100734310f3",
+            "title": "EBUS training.jpg",
+            "qname": "o:daa976116100734310f3",
+            "typeQName": "n:node"
+          },
+        },
+        {
+          "text": "this is the second string",
+        }
+
+      ]
+    };
+
+    const result = {
+      "image": "https://www.ersnet.org/assets/preview?node=daa976116100734310f3&name=img500&size=500",
+      "highResImage": "https://www.ersnet.org/assets/preview?node=daa976116100734310f3&name=img1800&size=1800",
+      "programme": "https://www.ersnet.org/assets/static?node=daa976116100734310f3",
+      "practicalInfo": "https://www.ersnet.org/assets/static?node=daa976116100734310f3",
+      "sponsor": [
+        {
+        "text": "this is the first string",
+        "image": "https://www.ersnet.org/assets/preview?node=daa976116100734310f3&name=img500&size=500"
+        },
+        {
+        "text": "this is the second string",
+        "image": "https://www.ersnet.org/assets/preview?node=daa976116100734310f3&name=img500&size=500"
+        },
+        {
+          "text": "this is the second string",
+        }
+      ]
+    };
+
+    const images = ['image', 'highResImage', 'sponsor']
+    const documents = ['practicalInfo', 'programme']
+    assert.deepEqual(format.parseAttachements(item, images, documents), result);
+  });
+
 });
